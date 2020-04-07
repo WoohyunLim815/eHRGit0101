35a4dba1256e43d57858a16b336eb42613abc0ad
sist <sist@DESKTOP-2RRLFEP>
2020-04-07 10:30:47 +0900

    커밋연습

--- eHRGit0101/src/com/sist/ehr/HelloWorld.java
+++ eHRGit0101/src/com/sist/ehr/HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, Git.");
 		System.out.println("Hello, Git01.");
-
+		System.out.println("정처기 필기그만하고 실기하세요.");
 	}
 
 }
