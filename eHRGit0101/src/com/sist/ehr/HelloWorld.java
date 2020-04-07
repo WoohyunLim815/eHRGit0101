c65d86581b813331b46ce0e82b0d48aceeeb081f
sist <sist@DESKTOP-FTFK184>
2020-04-07 09:51:16 +0900

    init

---
+++ eHRGit0101/src/com/sist/ehr/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.sist.ehr;
+
+/**
+ * @author sist
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, Git.");
+
+	}
+
+}
